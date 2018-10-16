5632ac9d82c7ff8cd7896ff2ebbdfb9043bca37c
sredorta <sergi.redorta@hotmail.com>
2018-10-16 19:32:18 +0200

    Update

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('products', 'ProductController@index');
 Route::get('products/{id}', 'ProductController@show');
-Route::post('products', 'ProductController@store');
-Route::put('products/{id}', 'ProductController@update');
-Route::delete('products/{id}', 'ProductController@delete');
+//Route::post('products', 'ProductController@store');
+//Route::put('products/{id}', 'ProductController@update');
+//Route::delete('products/{id}', 'ProductController@delete');
